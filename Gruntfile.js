1a2b56a421d01e09b72ca1536a1573481b249354
zenhase <zh@punyco.de>
2014-11-27 00:15:06 +0100

    Things start falling into place & I am getting the hang

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,19 +6,37 @@ module.exports = function (grunt) {
       server: {}
     },
     jshint: {
-      files: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js']
+      files: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']
     },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      scripts: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
+      'bower-install': {
+        files: ['<%= wiredep.task.src %>', 'bower.json'],
+        tasks: ['wiredep']
+      }
+    },
+    wiredep: {
+      task: {
+        src: ['app/**/*.html', '!app/bower_components/**/*.html'],
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+
+          // https://github.com/taptapship/wiredep#configuration
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-wiredep');
 
   grunt.registerTask('test', ['jshint']);
   grunt.registerTask('default', ['test']);
-  grunt.registerTask('serve', ['connect:server:keepalive'])
+  grunt.registerTask('serve', ['connect:server:keepalive']);
 };
